b236180c4313b2b82ae7cde77de4913705f0a7e9
Mayank-accionlabs <mayank.sahu@accionlabs.com>
2020-05-21 01:25:41 +0530

    istanbul coverage added for jest testing

---
+++ src/container/register/__test__/register.test.js
@@ -0,0 +1,11 @@
+// __tests__/CheckboxWithLabel-test.js
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import CheckboxWithLabel from '../Register';
+
+test('CheckboxWithLabel changes the text after click', () => {
+  // Render a checkbox with label in the document
+  expect(1).toEqual(1);
+
+});
